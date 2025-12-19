79404940ca46f1bdac90f16ce0109dc8a1598475
aradhya1a4 <arsharma1122187@gmail.com>
2025-12-19 14:11:03 +0000

    DONE

--- basicss/third.js
+++ basicss/third.js
@@ -4,3 +4,10 @@ console.log(typeof(stn))
 console.log(stn)
  console.log(1+2+"2")
  console.log("2"+2+2) // + se conversion ho jayega 
+ // stack memory primitive data
+ // heap memory non-primitive data types
+ // backticks string interpolation  basically it is f string
+ const gamename=new String("Aradhya")
+console.log(gamename.__proto__) // return object basically help to access to objectx
+console.log(gamename)
+ 
