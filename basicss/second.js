6e70aa18ba5e8878c9531ce3245ca9e84f2ace89
aradhya1a4 <arsharma1122187@gmail.com>
2025-12-19 10:23:56 +0000

    ready to committ

---
+++ basicss/second.js
@@ -0,0 +1,5 @@
+"use strict";// treat all js code as newer version helps js to avoid silent bugs\
+// null is standalone value
+// undefined value hi assign ni ki 
+// symbol is used in react when we need something unique
+// object 
